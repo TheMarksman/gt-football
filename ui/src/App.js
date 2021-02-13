068f8d24c389e743d1ae62d89cf37ab42fb77b96
Thomas Mark <tmm140@gmail.com>
2021-02-13 09:02:45 -0500

    added some more charts and ui component package

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,33 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
 import gtFootballData from './gt_football_data.json';
 import {
   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
+import { Container, Box, Flex, Heading } from '@chakra-ui/react';
 
 function App() {
   console.log(gtFootballData);
 
   return (
-    <div className="App">
-      <LineChart
-        width={500}
-        height={300}
-        data={gtFootballData}
-        margin={{
-          top: 5, right: 30, left: 20, bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="year" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="win" stroke="#2D882D" />
-        <Line type="monotone" dataKey="pointsPerGame" stroke="#226666" />
-        <Line type="monotone" dataKey="avgPointsPerGameAllowed" stroke="#AA3939" />
-      </LineChart>
-    </div>
+    <Container className="App" maxW="xl" centerContent>
+      <Flex>
+        <Box padding="4">
+          <Heading>Overall</Heading>
+          <LineChart
+            width={500}
+            height={300}
+            data={gtFootballData}
+            margin={{
+              top: 5, right: 30, left: 20, bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="year" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="win" stroke="#7fc97f" />
+            <Line type="monotone" dataKey="pointsPerGame" stroke="#386cb0" />
+            <Line type="monotone" dataKey="avgPointsPerGameAllowed" stroke="#f0027f" />
+          </LineChart>
+        </Box>
+
+        <Box padding="4">
+          <Heading>Offense</Heading>
+          <LineChart
+            width={500}
+            height={300}
+            data={gtFootballData}
+            margin={{
+              top: 5, right: 30, left: 20, bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="year" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="offYardsPerGame" stroke="#7fc97f" />
+            <Line type="monotone" dataKey="passYardsPerGame" stroke="#386cb0" />
+            <Line type="monotone" dataKey="rushingYardsPerGame" stroke="#fdc086" />
+          </LineChart>
+        </Box>
+
+        <Box padding="4">
+          <Heading>Defense</Heading>
+          <LineChart
+            width={500}
+            height={300}
+            data={gtFootballData}
+            margin={{
+              top: 5, right: 30, left: 20, bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="year" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="yardsPerGameAllowed" stroke="#f0027f" />
+            <Line type="monotone" dataKey="passYardsPerGameAllowed" stroke="#386cb0" />
+            <Line type="monotone" dataKey="rushYardsPerGameAllowed" stroke="#fdc086" />
+          </LineChart>
+        </Box>
+      </Flex>
+    </Container>
   );
 }
 
